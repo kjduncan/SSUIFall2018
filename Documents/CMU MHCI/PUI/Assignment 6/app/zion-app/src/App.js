cfa862cce9530ef2459fada914b75b8fb34ccf2d
kjduncan <katelynduncan@Katelyns-MBP.wv.cc.cmu.edu>
2018-10-23 12:22:53 -0400

    first commit

---
+++ Documents/CMU MHCI/PUI/Assignment 6/app/zion-app/src/App.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import Card from './components/card.js';
+import './App.css';
+import background from './images/zion.png';
+
+
+let trails = [
+  {
+    name: 'Angels Landing',
+    attributes: {
+      difficulty: '10',
+      duration: '10:00',
+      distance: 4.8
+    }
+  },
+  {
+    name: 'Narrows',
+    attributes: {
+      difficulty: 2,
+      duration: '2:12',
+      distance: 2.2
+    }
+  },
+  {
+    name: 'Grotto Trail',
+    attributes: {
+      difficulty: 2,
+      duration: '2:12',
+      distance: 2.2
+    }
+  },
+  {
+    name: 'Emerald Pools',
+    attributes: {
+      difficulty: 3,
+      duration: '2:12',
+      distance: 2.2
+    }
+  },
+  {
+    name: 'Weeping Rock',
+    attributes: {
+      difficulty: 4,
+      duration: '2:12',
+      distance: 2.2
+    }
+  },
+  {
+    name: 'Watchman Trail',
+    attributes: {
+      difficulty: 5,
+      duration: '2:12',
+      distance: 2.2
+    }
+  }
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {selectedTrail:{}};
+  }
+
+  selectedTrail = (trail) => {
+    // console.warn(trail);
+    this.setState({
+      selectedTrail: trail
+    });
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <nav>
+          <ul>
+            <button onClick={() => {this.selectedTrail(trails[0])}}><li>Angels Landing</li></button>
+            <button onClick={() => {this.selectedTrail(trails[1])}}><li>Narrows</li></button>
+            <button onClick={() => {this.selectedTrail(trails[2])}}><li>Grotto Trail</li></button>
+            <button onClick={() => {this.selectedTrail(trails[3])}}><li>Emerald Pools</li></button>
+            <button onClick={() => {this.selectedTrail(trails[4])}}><li>Weeping Rock</li></button>
+            <button onClick={() => {this.selectedTrail(trails[5])}}><li>Watchman Trail</li></button>
+          </ul>
+        </nav>
+        <div class="main" style={{backgroundImage: `url(${background})`}}>
+          <h1>Zion</h1>
+        </div>
+
+
+
+
+        {this.state.selectedTrail.name &&
+            <Card trail={this.state.selectedTrail} />
+        }
+
+      </div>
+    );
+  }
+}
+
+export default App;
